f3e66b5c01ba0ca25741492b9de75eafbcfb8069
BrianBoudrioux <brian1.boudrioux@epitech.eu>
2018-08-27 22:52:27 +0200

    fix

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Samsung Campus</title>
+  <title>SAMC - Organisation Platform</title>
 
   <link rel="icon" href="<?php echo URL ?>img/icon.png">
 
